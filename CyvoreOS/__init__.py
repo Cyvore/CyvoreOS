0b9c3f932ee19992265fa591e71b4a0e4a17dafc
barakinio <barakshfr@gmail.com>
2021-09-22 13:09:41 +0300

    Added __init__.py

---
+++ CyvoreOS/__init__.py
@@ -0,0 +1,4 @@
+from CyvoreOS.Plugins import _opr_plugin, testfav_plugin, virusTotal_plugin, abuseIPDB_plugin, cryptoCurrency_plugin
+from CyvoreOS.interfaces import outlook_intgration, right_click_install
+from CyvoreOS.Utils import CheckImpersonation
+from CyvoreOS import checkObject

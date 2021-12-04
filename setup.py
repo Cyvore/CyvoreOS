c55706a1a0ef9c53ccc0b585583328cf6c548f18
barakinio <barakshfr@gmail.com>
2021-12-04 16:51:39 +0200

    Fixed paths for windows and linux

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
+import os
+
+FOLDERNAME = "CyvoreOS"
 
 setup(
     name='CyvoreOS',
-    packages=['CyvoreOS', 'CyvoreOS\\Plugins', 'CyvoreOS\\Output', 'CyvoreOS\\interfaces'],
-    version='0.1.7',
+    packages=['CyvoreOS', os.path.join(FOLDERNAME, "Plugins"), os.path.join(FOLDERNAME, "Output"), os.path.join(FOLDERNAME, "interfaces")],
+    version='0.1.7.1',
     license='MIT',  #https://help.github.com/articles/licensing-a-repository
     description='Next-Gen email threat prevention',
     author='Cyvore',
     author_email='info@cyvore.com',
     url='https://github.com/user/reponame',  # Our website Link
-    download_url='https://github.com/barakinio/CyvoreOS/archive/refs/tags/v_0.1.7.tar.gz',  # Release Link
+    download_url='https://github.com/barakinio/CyvoreOS/archive/refs/tags/v_0.1.7.1.tar.gz',  # Release Link
     keywords=['Anti-Phishing', 'Email-Scanner', 'Optical-Recognition'],
     install_requires=[
         'levenshtein',

ff857e03d0d67d11652301837c63557cf92289de
barakinio <barakshfr@gmail.com>
2021-12-04 14:37:20 +0200

    Fixed Imports

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='CyvoreOS',
-    packages=['CyvoreOS'],
-    version='0.1.6',
+    packages=['CyvoreOS', 'CyvoreOS\\Plugins', 'CyvoreOS\\Output', 'CyvoreOS\\interfaces'],
+    version='0.1.7',
     license='MIT',  #https://help.github.com/articles/licensing-a-repository
     description='Next-Gen email threat prevention',
     author='Cyvore',
     author_email='info@cyvore.com',
     url='https://github.com/user/reponame',  # Our website Link
-    download_url='https://github.com/barakinio/CyvoreOS/archive/refs/tags/v_0.1.5.tar.gz',  # Release Link
+    download_url='https://github.com/barakinio/CyvoreOS/archive/refs/tags/v_0.1.7.tar.gz',  # Release Link
     keywords=['Anti-Phishing', 'Email-Scanner', 'Optical-Recognition'],
     install_requires=[
         'levenshtein',

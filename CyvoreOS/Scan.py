b6c3fdf5a0917acc32fb647d2f293b30ad2a9d62
Yoav Rotem <yoavrotems97@gmail.com>
2021-12-11 17:59:25 +0200

    Update Scan.py

--- CyvoreOS/Scan.py
+++ CyvoreOS/Scan.py
@@ -14,6 +14,7 @@ logging.basicConfig(filename='cyvore_main.log',
                     format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 printable = set(string.printable)
 
+# Get absolute path for plugins
 def iter_namespace(ns_pkg):
     # Specifying the second argument (prefix) to iter_modules makes the
     # returned name an absolute name instead of a relative one. This allows
@@ -21,12 +22,12 @@ def iter_namespace(ns_pkg):
     # the name.
     return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
 
+# Make all Plugins dictionary 
 discovered_plugins = {
     name: importlib.import_module(name)
     for finder, name, ispkg
     in iter_namespace(Plugins)
 }
-
 
 def exitWithLog(msg):
     if msg:
@@ -36,10 +37,8 @@ def exitWithLog(msg):
     logging.info("-----------------------------------------------------\n\n\n")
     exit()
 
-
 def strings(filename, min=4):
     with open(filename, errors="ignore") as f:  # Python 3.x
-    # with open(filename, "rb") as f:           # Python 2.x
         result = ""
         for c in f.read():
             if c in string.printable:
@@ -51,57 +50,69 @@ def strings(filename, min=4):
         if len(result) >= min:  # catch result at EOF
             yield result
 
-
 def listplugins():
     logging.info("Running ls option")
     for plugin in discovered_plugins:
         current_plugin = importlib.import_module(plugin)
         output = current_plugin.describe()
-        print("\t" + plugin, "-", output)
+        logging.info("\t" + plugin, "-", output)
     exitWithLog("")
-
-
+ 
+"""
+# Only for windows developers 
 def install():
     logging.info("Running install option")
     path = "\"C:\\Python39\\python.exe\" \"" + __file__ + "\" -uf \"%1\""
     logging.debug(f"Path in registry:\t{path}")
-    interfaces.right_click_install.define_action_on("*", "CheckForPhish", path, title="Run Phish Check")
-    print("Installed right click addon")
+    # interfaces.right_click_install.define_action_on("*", "CheckForPhish", path, title="Run Phish Check")
+    logging.info("Installed right click addon")
     exitWithLog("")
-
+"""
 
 def urlFromFileCommand(args):
-    testCase = Case()
+    testCase = Case(str(list(strings(args.urlFromFIle)))) 
     logging.info(f"Check file:\n\t{args.urlFromFIle}")
     logging.info(f"Create case:\n\t{testCase.caseID}")
-    print("Check file:\n\t", args.urlFromFIle)
-    print(f"Create case:\n\t{testCase.caseID}")
-    testCase.raw = str(list(strings(args.urlFromFIle)))
-    return testCase.createChecks()
+    return testCase
 
 
 def scanstring(string):
-    testCase = Case()
+    testCase = Case(string)
     logging.info(f"Check string:\n\t{string}")
     logging.info(f"Create case:\n\t{testCase.caseID}")
-    print("Check file:\n\t", string)
-    print(f"Create case:\n\t{testCase.caseID}")
-    testCase.raw = string
-    testCase.createChecks()
     for plugin in discovered_plugins:
         for chk in testCase.checkArray:
             current_plugin = importlib.import_module(plugin)
             current_plugin.run_check(chk)
+    return testCase
 
-
-def initmodule(command, arg):
-    testCase = Case()
-    if command != "qs":
-        return 500
-    else:
-        testCase.raw = arg
-        testCase.checkArray = testCase.createChecks()
+def runPlugins(plugins_list, testCase):
     for plugin in discovered_plugins:
+        if plugins_list:
+            plugin_name = str(plugin).split(".")[1].removesuffix("_plugin")
+            if plugin_name not in plugins_list:
+                logging.info(f"Skipping {plugin_name} - plugin flag is on.")
+                continue
+        elif "debug" in str(plugin).split(".")[1].removesuffix("_plugin") :
+            continue
+        logging.debug(f"Running plugin: {str(plugin)}")
         for chk in testCase.checkArray:
+            
             current_plugin = importlib.import_module(plugin)
             current_plugin.run_check(chk)
+        
+def process(stream):
+    found_str = ""
+    while True:
+        data = stream.read(1024*4)
+        if not data:
+            print("Not data")
+            break
+        for char in data:
+            if char in printable:
+                found_str += char
+            elif len(found_str) >= 4:
+                yield found_str
+                found_str = ""
+            else:
+                found_str = ""

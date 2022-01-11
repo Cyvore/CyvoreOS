2af6f6c72694fa722716b47ddb8f756e9a1c3a4a
barakinio <barakshfr@gmail.com>
2022-01-11 14:09:18 +0200

    updated version on setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ FOLDERNAME = "CyvoreOS"
 setup(
     name='CyvoreOS',
     packages=['CyvoreOS', os.path.join(FOLDERNAME, "Plugins"), os.path.join(FOLDERNAME, "Output"), os.path.join(FOLDERNAME, "interfaces")],
-    version='0.1.7.3.4',
+    version='0.1.7.4',
     license='MIT',  #https://help.github.com/articles/licensing-a-repository
     description='Next-Gen email threat prevention',
     author='Cyvore',
     author_email='info@cyvore.com',
     url='https://github.com/user/reponame',  # Our website Link
-    download_url='https://github.com/barakinio/CyvoreOS/archive/refs/tags/v_0.1.7.3.4.tar.gz',
+    download_url='https://github.com/barakinio/CyvoreOS/archive/refs/tags/v_0.1.7.4.tar.gz',
     keywords=['Anti-Phishing', 'Email-Scanner', 'Optical-Recognition'],
     install_requires=[
         'levenshtein',

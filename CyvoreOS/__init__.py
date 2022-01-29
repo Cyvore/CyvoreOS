4b5e7aca160c946e84e3b4da17f5fd6347694f06
barakinio <barakshfr@gmail.com>
2022-01-29 15:21:50 +0200

    Added new logic replacing ScanString

--- CyvoreOS/__init__.py
+++ CyvoreOS/__init__.py
@@ -3,4 +3,4 @@ from CyvoreOS.Plugins.abuseIPDB_plugin import run_check
 from CyvoreOS.Plugins.whois_plugin import run_check
 from CyvoreOS.Plugins.cryptoWalletValidator_plugin import run_check
 from CyvoreOS import checkObject
-from CyvoreOS.Scan import listplugins, scanstring
+from CyvoreOS.Scan import scanUrl, scan_one_string

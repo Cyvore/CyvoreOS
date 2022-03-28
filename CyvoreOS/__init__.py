2cc4a27bec4048414597ccceeb89de09fab46de5
PuyoPuyo <SpamFishTS@gmail.com>
2022-03-28 21:10:51 +0300

    Deleted old imports in __init__ so no api key is needed for CyvoreOS use

--- CyvoreOS/__init__.py
+++ CyvoreOS/__init__.py
@@ -1,6 +1,1 @@
-from CyvoreOS.Plugins.virusTotal_plugin import run_check
-from CyvoreOS.Plugins.abuseIPDB_plugin import run_check
-from CyvoreOS.Plugins.whois_plugin import run_check
-from CyvoreOS.Plugins.cryptoWalletValidator_plugin import run_check
-from CyvoreOS import checkObject
-from CyvoreOS.Scan import scanUrl, scan_one_string
+# This is an init file

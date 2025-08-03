f2dff25d59eb0cb67f5d39e6a3ba458f593f8085
Guy Avnon <guyavnon900@gmail.com>
2025-08-03 13:35:24 +0300

    added domain age and updated whois version

--- cyvoreos/plugins/whois_plugin.py
+++ cyvoreos/plugins/whois_plugin.py
@@ -1,7 +1,7 @@
 import datetime
 from pathlib import Path
 import logging
-from typing import Optional, Union
+from typing import Optional
 import whois
 import whois.whois
 from cyvoreos.plugins.base_plugin import BasePlugin
@@ -44,6 +44,8 @@ class WhoisPlugin(BasePlugin):
             host_dict = whois.whois(data)
             host_dict = dict(host_dict)
 
+            host_dict["domain_age"] = WhoisPlugin._calculate_domain_age(host_dict.get("creation_date"), logger)
+
             # change domain list from list to string
             if isinstance(host_dict.get("domain_name"), list):
                 host_dict["domain_name"] = host_dict["domain_name"][0].lower()
@@ -54,8 +56,8 @@ class WhoisPlugin(BasePlugin):
                 domain_list = open(file_path, "r", encoding="utf-8").read().split("\n")
 
             except Exception as e:
-                logger.warning(str(e))
-                logger.info("try use local file instead")
+                logger.debug(str(e))
+                logger.debug("try use local file instead")
                 p = Path(__file__).with_name("top500domains.txt")
                 domain_list = open(p, "r", encoding="utf-8").read().split("\n")
 
@@ -67,25 +69,32 @@ class WhoisPlugin(BasePlugin):
             logger.error("Error executing whois plugin", exc_info=e)
 
         return None
+    
+    @staticmethod
+    def _calculate_domain_age(creation_date: Optional[datetime.datetime], logger: logging.Logger = logging) -> Optional[int]:
+        """
+        Calculate the age of a domain name in days
+        """
+        if not creation_date:
+            logger.warning("No creation date found")
+            return None
+        
+        if isinstance(creation_date, list):
+            creation_date = creation_date[0]
+
+        return (datetime.datetime.now() - creation_date).days
 
     @staticmethod
     def _calculate_score(output: dict) -> float:
-        if output.get("creation_date", ""):
-            creation_date: Union[datetime.datetime, list[datetime.datetime]] = output.get("creation_date", "")
-
-            if not isinstance(creation_date, list):
-                creation_date = [creation_date]
-
-            for date in creation_date:
-                date = date.astimezone(datetime.timezone.utc)
-
-                # If the date is in the last 180 days, return 20.0
-                if date > (datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=180)):
-                    return 20.0
-                # If the date is in the last 365 days, return 10.0
-                elif date > (datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=365)):
-                    return 10.0
-
+        domain_age = output.get("domain_age")
+        if domain_age is None:
             return 0.0
+
+        if domain_age <= 7:
+            return 70.0
+        elif domain_age <= 30:
+            return 50.0
+        elif domain_age <= 90:
+            return 30.0
         else:
             return 0.0
